64ff2785bc3e50097bf47bdb26a0e1e84ec56979
thirusolai <96538671+thirusolai@users.noreply.github.com>
2025-08-24 20:47:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,4 +64,7 @@ app.post("/submit-form", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
